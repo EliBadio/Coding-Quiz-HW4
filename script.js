1abcd9cb95b3da7831d04c5c169a5c318e46c367
Elisha Badio <elisha.badio@uconn.edu>
2020-09-09 15:21:16 -0400

    update background  and time interval

--- script.js
+++ script.js
@@ -1,233 +1,198 @@
-var options001 = ["<button class=buttons001 onclick=q1i()>True</button><br /><br /><button class=buttons001 onclick=q1c()>False</button>"];
-var options002 = ["<button class=buttons001 onclick=q2c()>False</button><br /><br /><button class=buttons001 onclick=q2i()>True</button>"];
-var options003 = ["<button class=buttons001 onclick=q3c()>True</button><br /><br /><button class=buttons001 onclick=q3i()>False</button>"];
-var start01="",
-var a = 0;
-a++;
-var b = 0;
-b++;
-var question001 = ["What is a function that call itself.",
-"A regular binding where the value of the biding is a function.",
-"When statements on a program are executed from top to botton, it is an example of.","What type of data use to represent text?",
-"Blank is a fragment that produces a value?"
-]
+//------------------------------------------------------------------------------------------
+var countDown;
 
-function start01() {
-    c = 200;
-    disappear001.innerHTML = "";
-    message001.innerHTML = question001[0];
-    message002.innerHTML = options001;
-    number001.innerHTML = a++;
-    }
-    
-    function q1c() {
-    message003.innerHTML = "Correct. Function.";
-    message002.innerHTML = "";
-    score001.innerHTML = a++;
-    message004.innerHTML = "<button class=buttons002 onclick=next001()>Next</button>";
-    }
-    
-    function q1i() {
-    message003.innerHTML = "Incorrect.function.";
-    message002.innerHTML = "";
-    message004.innerHTML = "<button class=buttons002 onclick=next001()>Next</button>";
-    }
-    
-    function q2c() {
-    message003.innerHTML = "Correct. Recursion.";
-    message002.innerHTML = "";
-    score001.innerHTML = a++;
-    message004.innerHTML = "<button class=buttons002 onclick=next001()>Next</button>";
-    }
-    
-    function q2i() {
-    message003.innerHTML = "Incorrect. recursion.";
-    message002.innerHTML = "";
-    message004.innerHTML = "<button class=buttons002 onclick=next001()>Next</button>";
-    }
-    
-    function q3c() {
-    message003.innerHTML = "Correct. string";
-    message002.innerHTML = "";
-    score001.innerHTML = b++;
-    message004.innerHTML = "<button class=buttons002 onclick=next001()>Next</button>";
-    }
-    
-    function q3i() {
-    message003.innerHTML = "Incorrect. string.";
-    message002.innerHTML = "";
-    message004.innerHTML = "<button class=buttons002 onclick=next001()>Next</button>";
-    }
-    
-    
-    function next001() {
-    if (a == "2") {
-        message001.innerHTML = question001[1];
-        message002.innerHTML = options002;
-        message003.innerHTML = "";
-        number001.innerHTML = a++;
-        message004.innerHTML = "";
-    }
-    
-    else if (a == "3") {
-        message001.innerHTML = question001[2];
-        message002.innerHTML = options003;
-        message003.innerHTML = "";
-        number001.innerHTML = a++;
-        message004.innerHTML = "";
-    }
-    
-    }
-    
-    update = setInterval("timer001()", 1000);
-    
-    function repeat001() {
-    location.reload();
+// pos is position of where the user in the test or which question they're up to
+var pos = 0,
+  test,
+  test_status,
+  question,
+  choice,
+  choices,
+  chA,
+  chB,
+  chC,
+  correct = 0;
+// this is a multidimensional array with 4 inner array elements with 5 elements inside them
+var questions = [
+  {
+    question: 'What is a function that call itself?',
+    a: 'Array',
+    b: 'Recursion',
+    c: 'Object',
+    answer: 'B',
+  },
+  {
+    question: 'A regular binding where the value of the biding is a function?',
+    a: 'Recursion',
+    b: 'Array',
+    c: 'Function',
+    answer: 'C',
+  },
+  {
+    question:
+      'When statements on a program are executed from top to botton, it is an example of?',
+    a: 'Control Flow',
+    b: 'Object',
+    c: 'Map Flow',
+    answer: 'A',
+  },
+  {
+    question: 'Blank is a fragment that produces a value?',
+    a: 'Binding',
+    b: 'Array',
+    c: 'Expression',
+    answer: 'C',
+  },
+  {
+    question: '___________ are nothing more than properties that hold function values',
+    a: 'Array',
+    b: 'Recursion',
+    c: 'Object',
+    answer: 'B',
+  },
+  {
+    question: 'A measure of dependency between statistical variation is called?',
+    a: 'Recursion',
+    b: 'Array',
+    c: 'Correlation',
+    answer: 'C',
+  },
+  {
+    question:
+      'The _______ keyword generally refers to the object it belongs and could have different meaning?',
+    a: 'this',
+    b: 'Object',
+    c: 'Map Flow',
+    answer: 'A',
+  },
+  {
+    question: 'All of these are keyword reserved forjavascript, except?',
+    a: 'abstract',
+    b: 'break',
+    c: 'defile',
+    answer: 'C',
+  },
+  {
+    question:
+      'When an operator is ________, it means that the operator only has a single operand?',
+    a: 'Unary',
+    b: 'Binary',
+    c: 'Ternay',
+    answer: 'A',
+  },
+  {
+    question: 'When an operator is ________, it means that the operator only has a single operand',
+    a: 'Unary',
+    b: 'Ternary',
+    c: 'Binary',
+    answer: 'C',
+  },
+];
+// this get function is short for the getElementById function
+function get(x) {
+  return document.getElementById(x);
+}
 
-function check(){
-var question1= document.quiz.question1.value;
-var question2= document.quiz.question2.value;
-var question3= document.quiz.question3.value;
-var question4= document.quiz.question4.value;
-var question5= document.quiz.question5.value;
-var correct = 0;
+// this function renders a question for display on the page
+function renderQuestion() {
+  test = get('test');
+  if (pos >= questions.length) {
+    test.innerHTML =
+      '<h2>You got ' +
+      correct +
+      ' of ' +
+      questions.length +
+      ' questions correct</h2>';
+    get('test_status').innerHTML = 'Test completed';
+    // resets the variable to allow users to restart the test
+    pos = 0;
+    correct = 0;
+    // stops rest of renderQuestion function running when test is completed
+    return false;
+  }
+  get('test_status').innerHTML =
+    'Question ' + (pos + 1) + ' of ' + questions.length;
 
-if( question1 ==" Recurcion"){
+  question = questions[pos].question;
+  chA = questions[pos].a;
+  chB = questions[pos].b;
+  chC = questions[pos].c;
+  // display the question
+  test.innerHTML = '<h3>' + question + '</h3>';
+  // display the answer options
+  // the += appends to the data we started on the line above
+  test.innerHTML +=
+    "<label> <input type='radio' name='choices' value='A'> " +
+    chA +
+    '</label><br>';
+  test.innerHTML +=
+    "<label> <input type='radio' name='choices' value='B'> " +
+    chB +
+    '</label><br>';
+  test.innerHTML +=
+    "<label> <input type='radio' name='choices' value='C'> " +
+    chC +
+    '</label><br><br>';
+  test.innerHTML += "<button onclick='checkAnswer()'>Submit Answer</button>";
+}
 
+function checkAnswer() {
+  // use getElementsByName because we have an array which it will loop through
+  choices = document.getElementsByName('choices');
+  for (var i = 0; i < choices.length; i++) {
+    if (choices[i].checked) {
+      choice = choices[i].value;
+    }
+  }
+  // checks if answer matches the correct choice
+  if (choice == questions[pos].answer) {
+    //each time there is a correct answer this value increases
     correct++;
+  }
+  // changes position of which character user is on
+  pos++;
+  // then the renderQuestion function runs again to go to next question
+  renderQuestion();
+
+  // hide timer when quiz is completed
+  if (pos === 0) {
+    document.getElementById('displayTime').style.display = 'none';
+  }
 }
 
-if( question2=="Function"){
+// diplay time countdown
+function startQuiz() {
+  // Display time countdown
+  document.getElementById('displayTime').innerHTML =
+    'Time:<text id="time01">50.00</text></text>';
 
-    correct++;
+  countDown = 50.00;
+
+  var interval = setInterval(function () {
+    time01.innerHTML = countDown;
+    countDown--;
+    if (countDown === 0) {
+      clearInterval(interval);
+      time01.innerHTML = 'Time Up!!!';
+      time01.style.color = 'red';
+
+      // Display result
+      test.innerHTML =
+        '<h2>You got ' +
+        correct +
+        ' of ' +
+        questions.length +
+        ' questions correct</h2>';
+    }
+  }, 1000);
+
+  // Hide start button
+  document.getElementById('start').style.display = 'none';
+
+  renderQuestion();
 }
 
-f( question3=="Control"){
+// Add event listener (to start button) to call start quiz and renderQuestion
+document.getElementById('start').addEventListener('click', startQuiz);
 
-    correct++;
-}
-
-f( question4=="String"){
-
-    correct++;
-}
-f( question4=="Expression"){
-
-    correct++;
-}
-
-
-var range;
-
-if ( correct = 1){
-    
-if (correct> 0 && correct< 3){
- 
-}
-
-if (correct>2){
-  
-}
-
-    document.getElementById("after_Submit").style.visibility="visible"
-
-    document.getElementById("message").innerHTML = messages[range];
-    document.getElementById("number_Correct").innerHTML= "You got" + Correct + "correct";
-    document.getElementById("picture").src=pictures[range];
-    
-
-}
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*// in this below example, we created a variable questions we asked three questions
-
-var questions= [" what is 2+2"," what is 4+2", " what is 4+9"];
-// then we store the correct answer under the variable correctAnswer
-var correctAnswers=[ 4,6,9];
-
-// here we loop the answer given by the user and making we stop the application after 3 answers
-for (i = 0; i<3; i++)
-{
-    //here we change capture the first answer and the first questions also display
-var answer= Number(prompt(questions[i],""));
-
-// here we comparing the answer with the correctAnswer that we stored
-
-if (answer== correctAnswers[i]) alert("Well Done!!");else alert("Sorry wrong answer");
-
-}
-
-
-// below example is not working correctly because the application is not looping for correctAnswer
-// however the wrong answers are looping to the end of the application
-
-// in this below example, we created a variable questions we asked three questions
-var questions= [" what is 2+2"," what is 4+2", " what is 4+9"];
-// then we store the correct answer under the variable correctAnswer
-var correctAnswers=[ 4,6,9];
-
-// here we added the score
-var score =0;
-for (i = 0; i<3; i++)
-{
-var answer= Number(prompt("Question"+(i+1)+":"+questions[i],""));
-if( answer==correctAnswers[i])
-{
-aler("Well Done");
-score++;
-}
-else
-{
-alert("Sorry wrong answer");
-}
-}
-
-alert("Your answered" + score + " questions correctly");*/
+//-------------------------------------------------------------------------------
